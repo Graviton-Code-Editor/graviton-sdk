57fcc9183f9c93ccba53c6ac53d0c963b627ca3d
Marc Espín Sanz <mespinsanz@gmail.com>
2020-04-24 23:24:46 +0200

    :sparkles: You can now build themes

--- index.js
+++ index.js
@@ -36,46 +36,53 @@ function validateMode( mode ){
 
 function bundleParcel(entryProject){
 	entryProject = path.join(process.cwd(),entryProject)
-	return new Promise(async(resolve,reject)=>{
+	return new Promise(async (resolve,reject) => {
 		let error = false
 		try {
-			const entryPackage = require(entryProject)
-			} catch(data){
-				error = true
-			}
-		if( error ) {
-			console.error(error)
-			return
+			const entryPackage = require(entryProject);
+		} catch(data){
+			error = true
 		}
+		if( error ) return console.error(error)
 		const entryPackage = require(entryProject)
+		const pluginType = entryPackage.type || 'plugin'
 		const entryFolder = path.dirname(entryProject)
 		const entryDir =  path.resolve(__dirname,entryFolder)
-		const entryFile = path.join(entryDir,entryPackage.mainSrc)
 		const dirFolder = path.join(entryDir,'dist')
-		const cacheFolder = path.join(entryFolder,'.cache')
-		const bundler = new Bundler(entryFile, {
-			outDir: dirFolder, 
-			cache: true, 
-			watch: false,
-			cacheDir: cacheFolder, 
-			contentHash: false,
-			minify: true, 
-			target: 'node',
-			bundleNodeModules: true, 
-			sourceMaps: true, 
-			detailedReport: false 
-		})
-		bundler.bundle();
-		bundler.on('bundled',()=>{
-			setTimeout(()=>{
-				resolve({
-					entryPackage,
-					entryDir,
-					entryFile,
-					dirFolder
-				})
-			},4000)
-		})
+		if ( pluginType == 'plugin' ){
+			const entryFile = path.join(entryDir,entryPackage.mainSrc)
+			const cacheFolder = path.join(entryFolder,'.cache')
+			const bundler = new Bundler(entryFile, {
+				outDir: dirFolder, 
+				cache: true, 
+				watch: false,
+				cacheDir: cacheFolder, 
+				contentHash: false,
+				minify: true, 
+				target: 'node',
+				bundleNodeModules: true, 
+				sourceMaps: true, 
+				detailedReport: false 
+			})
+			bundler.bundle();
+			bundler.on('bundled',()=>{
+				setTimeout(()=>{
+					resolve({
+						pluginType,
+						entryPackage,
+						entryDir,
+						dirFolder
+					})
+				},4000)
+			})
+		} else {
+			resolve({
+				pluginType,
+				entryPackage,
+				entryDir,
+				dirFolder
+			})
+		}
 	})
 }
 
@@ -107,9 +114,10 @@ function copyPackageToDist({
 	entryProject,
 	dirFolder
 }){
-	return new Promise((resolve,reject)=>{
+	return new Promise((resolve,reject) => {
 		const dirProject = path.join(dirFolder,'package.json')
-		fs.copyFile(entryProject, dirProject, (err) => {
+		if( !fs.existsSync(dirFolder) ) fs.mkdirSync(dirFolder)
+		fs.copyFile(entryProject, dirProject, err => {
 			if (err) throw err;
 			resolve()
 		});
@@ -121,7 +129,7 @@ function bundleZip({
 	dirFolder,
 	entryPackage
 }){
-	return new Promise(async (resolve,reject)=>{
+	return new Promise(async (resolve,reject) => {
 		const buildFolder = path.join(entryDir,'build')
 		if( !fs.existsSync(buildFolder) ) fs.mkdirSync(buildFolder)
 		const buildDir = path.join(buildFolder,`${entryPackage.name}.zip`)
@@ -132,15 +140,15 @@ function bundleZip({
 	})
 }
 
-(async()=> {
+(async () => {
 	const target = validateTarget(program.target)
 	const mode = validateMode(program.mode)
 	if( target && program.entry ){
 		switch(mode){
 			case 'release':
-				let { dirFolder, entryDir, entryPackage } = await bundleParcel(program.entry)
+				let { dirFolder, entryDir, entryPackage, pluginType } = await bundleParcel(program.entry)
 				await copyPackageToDist({
-					entryProject:program.entry,
+					entryProject: program.entry,
 					dirFolder
 				})
 				let { buildDir } = await bundleZip({
---
+++ test/src/main.js
@@ -0,0 +1,3 @@
+export const entry = ()=>{
+	console.log('Works.')
+}
